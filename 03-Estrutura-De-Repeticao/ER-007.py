0e50f7820c8a111e431c8c110549be86b4ecc60f
cauanvital <cauanvital74@gmail.com>
2023-08-19 20:59:43 -0300

    Exercício de Estrutura de Decisão

---
+++ 03-Estrutura-De-Repeticao/ER-007.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia 5 números e informe o maior número.
+
+print(-1 > -6)
+n = []
+maior = 0
+for i in range(5):
+    n.append(int(input(f"Insira o {i + 1} número inteiro:   ")))
+    if i == 0:
+        maior = n[0]
+    if n[i] > maior:
+        maior = n[i]
+
+print(f"O maior valor digitado foi {maior}")
