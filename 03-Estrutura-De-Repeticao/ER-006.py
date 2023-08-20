6910a9e9694ea5d3345b15694d05b2b2c10cd860
cauanvital <cauanvital74@gmail.com>
2023-08-19 21:24:56 -0300

    Exercício de Estrutura de Decisão

--- 03-Estrutura-De-Repeticao/ER-006.py
+++ 03-Estrutura-De-Repeticao/ER-006.py
@@ -1,13 +1,11 @@
 # Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que
 # ele mostre os números um ao lado do outro.
 
-for i in range(20):
-    i += 1
+for i in range(1, 21):
     print(i)
 
 r = ""
-for i in range(20):
-    i += 1
+for i in range(1, 21):
     r = f"{r} {i}"
 
 print(r)
--- 03-Estrutura-De-Repeticao/ER-008.py
+++ 03-Estrutura-De-Repeticao/ER-008.py
@@ -1,8 +1,8 @@
 # Faça um programa que leia 5 números e informe a soma e a média dos números.
 
 soma = 0
-for i in range(5):
-    n = float(input(f"Insira o {i + 1}º número:   "))
+for i in range(1, 6):
+    n = float(input(f"Insira o {i}º número:   "))
     soma += n
 
 media = soma / 5
